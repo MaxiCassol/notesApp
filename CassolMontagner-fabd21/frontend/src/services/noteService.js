eece1a6e1feaecee2168af67b80c1b1ebfc11fec
Maximiliano Cassol Montagner <maximilianocassol@gmail.com>
2025-03-03 11:53:42 -0300

    production url

--- CassolMontagner-fabd21/frontend/src/services/noteService.js
+++ CassolMontagner-fabd21/frontend/src/services/noteService.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/notes';
+// DEV URL
+// const API_URL = 'http://localhost:3000/notes';
+
+// PRODUCTION URL
+const API_URL = 'https://notes-app-six-wine.vercel.app/';
 
 export const getNotes = async () => {
   const response = await axios.get(API_URL);
